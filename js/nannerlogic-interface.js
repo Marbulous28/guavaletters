ea64eb8ca6bdf24d6a66918a10f801df78bec083
Nick Arrasate and Peter Beach <nick+peter@>
2016-06-20 14:49:45 -0700

    working logic for reading columns and rows.

--- js/nannerlogic-interface.js
+++ js/nannerlogic-interface.js
@@ -3,9 +3,11 @@ var NannerLogic = require("./../js/nannerlogic.js").NannerLogic;
 $(document).ready(function(){
   var nannerLogic = new NannerLogic();
   $('#run').click(function(){
-    var testArray = nannerLogic.checkArray();
-    for(var i = 0; i < testArray.length; i++){
-      var enteredWord = testArray[i];
+    var testArrayRows = nannerLogic.checkArrayRows();
+    var testArrayCols = nannerLogic.columnsToRows();
+    console.log(testArrayCols);
+    for(var i = 0; i < testArrayRows.length; i++){
+      var enteredWord = testArrayRows[i];
       var api = 'http://api.pearson.com/v2/dictionaries/entries?headword=' + enteredWord;
       $.get(api, function(response){
         if(response.results.length !== 0){
--- js/nannerlogic.js
+++ js/nannerlogic.js
@@ -1,18 +1,22 @@
 exports.NannerLogic = function() {
-  this.nannerArray = [null, null, null, "h", "i","t", null,"i", "t", null, "t","o","p", null, null, "h","o","w",null];
+  this.rowArray1 = ['1', '3', null, "h", "b","t", null, "h","o","w",null];
+  this.rowArray2 = ['2', '4', "s","h","u","r",null,null,"u","p",null];
+  this.rowArray3 = ['5','6',"f","r","t","h","l","j","m","l",null];
+  this.masterArray = [this.rowArray1, this.rowArray2, this.rowArray3];
   this.wordArray = [];
   this.ryansArray = [];
   this.nullSwitch = false;
+  this.columnArray = []
 };
 
-exports.NannerLogic.prototype.checkArray = function ( ) {
-
-    for(var i = 0; i < this.nannerArray.length; i++){
-      if(this.nannerArray[i] !== null && this.nullSwitch == true){
-        this.wordArray.push(this.nannerArray[i]);
+exports.NannerLogic.prototype.checkArrayRows = function () {
+  for(var x = 0; x < this.masterArray.length; x ++){
+    for(var i = 0; i < this.masterArray[x].length; i++){
+      if(this.masterArray[x][i] !== null && this.nullSwitch == true){
+        this.wordArray.push(this.masterArray[x][i]);
         this.nullSwitch = false;
-      } else if (this.nannerArray[i] !== null){
-        this.wordArray.push(this.nannerArray[i]);
+      } else if (this.masterArray[x][i] !== null){
+        this.wordArray.push(this.masterArray[x][i]);
       } else {
         if(this.wordArray.length === 0){
           this.nullSwitch = true;
@@ -22,5 +26,34 @@ exports.NannerLogic.prototype.checkArray = function ( ) {
         }
       }
     };
-    return this.ryansArray;
+  };
+  return this.ryansArray;
+}
+
+exports.NannerLogic.prototype.columnsToRows = function () {
+  for (var i = 0; i < 11; i++) {
+    for (var x = 0; x < this.masterArray.length; x++) {
+      this.columnArray.push(this.masterArray[x][i]);
+    }
+  }
+return this.columnArray;
+
+
+for(var i = 0; i < this.columnArray.length; i++){
+  if(this.columnArray[i] !== null && this.nullSwitch == true){
+    this.wordArray.push(this.columnArray[i]);
+    this.nullSwitch = false;
+  } else if (this.columnArray[i] !== null){
+    this.wordArray.push(this.columnArray[i]);
+  } else {
+    if(this.wordArray.length === 0){
+      this.nullSwitch = true;
+    } else {
+      this.ryansArray.push(this.wordArray.join(''));
+      this.wordArray = [];
+    }
+  }
+};
+return this.ryansArray;
+console.log(ryansArray);
 }
