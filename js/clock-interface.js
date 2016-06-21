5beefc67967acf2cd6d0f9b46bf9a849c3682599
Ryan Harvey and Samuel Durfey <ryan@>
2016-06-21 09:13:32 -0700

    Alter clock to start when run is pressed.
    Clock also no longer has compounding click issue where pressing it more than once makes time drain faster.

--- js/clock-interface.js
+++ js/clock-interface.js
@@ -2,17 +2,29 @@ var moment = require("./../bower_components/moment/moment.js");
 // var Clock = require("./../js/clock.js").Clock;
 
 $(document).ready(function() {
+  $("#timer").hide();
   var $counter = 0;
 
   var update = function(htmlElement) {
     var currentTime = moment().hour(1).minute(0).second($counter--).format('HH : mm : ss');
     htmlElement.text(currentTime);
   }
+  var updateInterval;
+
 
   // var timer = new Clock();
   // timer.counter = 0;
   update($("time"));
-  $("#start").click( function() {
-    setInterval(update, 1000, $("time"));
+  $("#run").one("click", function() {
+    $("#timer").show();
+    updateInterval = setInterval(update, 1000, $("time"));
+    $("#stop").one("click", function() {
+      clearInterval(updateInterval);
+      // add another start listener.
+      // $("#start").one("click", function() {
+      //   updateInterval = setInterval(update, 1000, $("time"));
+      // });
+    });
   });
+
 });
