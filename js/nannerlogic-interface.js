f78e4f6e96ae5d0cba31ae8c358e70f95cbe1b7f
Samuel Durfey <samuel.durfey@gmail.com>
2016-06-23 11:39:11 -0700

    Check for letters used and all letters connected before checking.

--- js/nannerlogic-interface.js
+++ js/nannerlogic-interface.js
@@ -1,14 +1,15 @@
 var NannerLogic = require("./../js/nannerlogic.js").NannerLogic;
+var nannerLogic = new NannerLogic();
 var clickedTile = null;
 var dumpCounter = 0;
 var testArrayRows = [];
 var testArrayCols = [];
 var testArrayAll = [];
+var newLetter;
 
 $(document).ready(function(){
   $('#run').click(function(){
     $('.intro-screen').hide();
-    var nannerLogic = new NannerLogic();
     $('.gameBoard').slideDown();
     $('.handDisplay').slideDown();
     $('#run').hide();
@@ -48,7 +49,7 @@ $(document).ready(function(){
         if (dumpCounter < 2) {
           $(ui.draggable).remove();
           dumpCounter++;
-          var newLetter = nannerLogic.letterGenerator();
+          newLetter = nannerLogic.letterGenerator();
           $('#handRow4').append("<span id='newTile" + dumpCounter + "' class='gameTile col-xs-1'>" + newLetter + "</span>");
           $('.gameTile').draggable({
             snap: '.boardTile',
@@ -57,49 +58,48 @@ $(document).ready(function(){
           });
         } else if (dumpCounter === 2){
           $(ui.draggable).remove();
-          var newLetter = nannerLogic.letterGenerator();
+          newLetter = nannerLogic.letterGenerator();
           $('#handRow4').append("<span id='newTile" + dumpCounter + "' class='gameTile col-xs-1'>" + newLetter + "</span>");
           $('.gameTile').draggable({
             snap: '.boardTile',
             snapMode: 'inner',
             revert: 'invalid'
           });
-          $('.dump').hide()
+          $('.dump').hide();
         }
       }
     });
 
-    $('#submit').click(function(){
-      testArrayAll = [];
-      testArrayRows = [];
-      testArrayCols = [];
-      for (var xx=1; xx<=400; xx++){
-        nannerLogic.masterRowArray.push($('#' + xx).text());
-      }
 
-      for (var yy=1; yy<=20; yy++){
-        for (var zz=1; zz<=20; zz++){
-          nannerLogic.masterColArray.push($('.row'+ zz + ' .col' + yy).text());
-        }
-      }
-      var lettersConnected = nannerLogic.lettersConnected();
-      console.log(lettersConnected);
-      console.log(nannerLogic.masterRowArray, "row array");
-      testArrayRows = nannerLogic.checkArrayRows();
-      testArrayCols = nannerLogic.columnsToRows();
-      var lettersUsed = nannerLogic.checkLetters(testArrayCols);
-      console.log(lettersUsed, "are the letters used?");
-      testArrayAll = testArrayRows.concat(testArrayCols);
-      var enteredWord = '';
+  }); //run
 
-      console.log(testArrayAll);
-      for(var i = 0; i < testArrayAll.length; i++){
-        var notWordArray = [];
-        if(testArrayAll[i].length > 1){
-          enteredWord = testArrayAll[i];
-          // var api = 'http://api.pearson.com/v2/dictionaries/entries?headword=' + enteredWord;
-          $.get("./../../SOWPODS.txt").then(function(response) {
-            var dict = response.split("\n");
+  $('#submit').click(function(){
+    testArrayAll = [];
+    testArrayRows = [];
+    testArrayCols = [];
+    for (var xx=1; xx<=400; xx++){
+      nannerLogic.masterRowArray.push($('#' + xx).text());
+    }
+
+    for (var yy=1; yy<=20; yy++){
+      for (var zz=1; zz<=20; zz++){
+        nannerLogic.masterColArray.push($('.row'+ zz + ' .col' + yy).text());
+      }
+    }
+    var lettersConnected = nannerLogic.lettersConnected();
+    testArrayRows = nannerLogic.checkArrayRows();
+    testArrayCols = nannerLogic.columnsToRows();
+    var lettersUsed = nannerLogic.checkLetters(testArrayCols);
+    testArrayAll = testArrayRows.concat(testArrayCols);
+    var enteredWord = '';
+
+    if (lettersConnected && lettersUsed) {
+      $.get("./../../SOWPODS.txt").then(function(response) {
+        var dict = response.split("\n");
+        for(var i = 0; i < testArrayAll.length; i++){
+          var notWordArray = [];
+          if(testArrayAll[i].length > 1){
+            enteredWord = testArrayAll[i];
             if( dict.indexOf(enteredWord) !== -1 ){
               console.log(enteredWord + ' is a real word. nice job!');
               $('gameBoard').hide();
@@ -108,9 +108,10 @@ $(document).ready(function(){
               notWordArray.push(enteredWord);
               console.log(enteredWord, notWordArray);
             }
-          });
+          }
         }
-      }
-    });
+      });
+    }
   });
-});
+
+}); //ready
