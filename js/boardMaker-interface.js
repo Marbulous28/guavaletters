4abc9ce0d2e476705fdf35bd93a2b6d924942996
Samuel Durfey <samuel.durfey@gmail.com>
2016-06-23 10:31:20 -0700

    Adds dictionary search function. holy crap, I think it works.

--- js/boardMaker-interface.js
+++ js/boardMaker-interface.js
@@ -1,8 +1,11 @@
 var BoardMaker = require("./../js/boardMaker.js").BoardMaker;
-var Dictionary = require("./../js/dictionary.js").Dictionary;
-var dictionary = new Dictionary();
+// var Dictionary = require("./../js/dictionary.js").Dictionary;
+// var dictionary = new Dictionary();
 $(document).ready(function() {
+  // dictionary.testDict();
+  // $("#dictTest").click(function() {
+  //   dictionary.search('test');
+  // });
   var boardMaker = new BoardMaker();
   boardMaker.makeBoard();
-  dictionary.testDict();
 });
--- js/dictionary.js
+++ js/dictionary.js
@@ -1,4 +1,8 @@
-exports.Dictionary = function() {};
+exports.Dictionary = function() {
+  // this.dict = $.get("./../../SOWPODS.txt").then(function(response) {
+  //   return response.split("\n");
+  // });
+};
 
 exports.Dictionary.prototype.testDict = function() {
   $.get("./../../SOWPODS.txt").then(function(response) {
@@ -6,3 +10,16 @@ exports.Dictionary.prototype.testDict = function() {
     console.log(dict[37]);
   });
 };
+
+exports.Dictionary.prototype.search = function(input) {
+  var word = input.toUpperCase();
+  $.get("./../../SOWPODS.txt").then(function(response) {
+    var dict = response.split("\n");
+    console.log(dict.indexOf(word) !== -1 ? true : false);
+  });
+
+  // var word = input.toUpperCase();
+  // var isItThere = this.dict.indexOf(word) !== (-1) ? true : false ;
+  // console.log(isItThere);
+  // return isItThere;
+}
